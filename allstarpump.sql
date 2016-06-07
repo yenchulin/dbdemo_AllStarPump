c1f7179841fb2a65613ae6bad341195ff31fac82
yenchulin <103306003@nccu.edu.tw>
2016-06-07 20:42:41 +0800

    modified sql

--- allstarpump.sql
+++ allstarpump.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- 主機: localhost
--- 產生時間： 2016 年 06 月 07 日 13:21
--- 伺服器版本: 5.5.42
--- PHP 版本： 7.0.0
+-- 主機: 127.0.0.1
+-- 產生時間： 2016-06-07 14:42:11
+-- 伺服器版本: 10.1.10-MariaDB
+-- PHP 版本： 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,80 +17,31 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `Allstarpump`
+-- 資料庫： `allstarpump`
 --
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `aCompany`
+-- 資料表結構 `assemble`
 --
 
-CREATE TABLE `aCompany` (
-  `firmName` varchar(10) NOT NULL,
-  `componentId` varchar(10) NOT NULL,
-  `componentName` varchar(20) NOT NULL,
-  `componentQuantity` int(3) NOT NULL,
-  `address` varchar(30) NOT NULL,
-  `phoneNo` int(10) NOT NULL,
-  `personIC` varchar(5) NOT NULL,
-  `lendOT` datetime NOT NULL,
-  `returnT` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `Assemble`
---
-
-CREATE TABLE `Assemble` (
+CREATE TABLE `assemble` (
   `firmA` varchar(10) NOT NULL,
   `firmB` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- 資料表結構 `bCompany`
+-- 已匯出資料表的索引
 --
 
-CREATE TABLE `bCompany` (
-  `firmName` varchar(10) NOT NULL,
-  `pumpId` varchar(20) NOT NULL,
-  `pumpUsage` text NOT NULL,
-  `pumpQuantity` int(3) NOT NULL,
-  `pumpCategory` varchar(50) NOT NULL,
-  `pumpType` varchar(30) NOT NULL,
-  `pumpFlow` int(10) NOT NULL,
-  `address` varchar(30) NOT NULL,
-  `phoneNo` int(10) NOT NULL,
-  `personIC` varchar(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
 --
--- 資料表結構 `member`
+-- 資料表索引 `assemble`
 --
-
-CREATE TABLE `member` (
-  `account` varchar(15) NOT NULL,
-  `password` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `module`
---
-
-CREATE TABLE `module` (
-  `moduleId` varchar(20) NOT NULL,
-  `moduleUsage` text NOT NULL,
-  `moduleQuantity` int(3) NOT NULL,
-  `currentLocation` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE `assemble`
+  ADD PRIMARY KEY (`firmA`,`firmB`),
+  ADD UNIQUE KEY `firmB` (`firmB`),
+  ADD UNIQUE KEY `firmA` (`firmA`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
