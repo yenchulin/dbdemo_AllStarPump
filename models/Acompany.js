7ca0b23f97142faefe27ac0433be89a35b20456c
yenchulin <103306003@nccu.edu.tw>
2016-06-09 22:08:53 +0800

    add Acom

--- models/Acompany.js
+++ models/Acompany.js
@@ -1,13 +1,11 @@
 var db = require('../libs/db'); //引入我們的sql builder
 var GeneralErrors = require('../errors/GeneralErrors');
 
-
-var acom = function(options) {
+var Acompany = function(options) {
   this.firmName = options.firmName;
   this.componentID = options.componentID;
   this.componentName = options.componentName;
   this.componentQuantity = options.componentQuantity;
-  this.pumpCategory = options.pumpCategory;
   this.address = options.address;
   this.phoneNo = options.phoneNo;
   this.personIC = options.personIC;
@@ -17,98 +15,92 @@ var acom = function(options) {
 
 
 //Class Function
-acom.get = function(acomName, cb) {
-  //這邊是當傳入一個memberId時，進入資料庫查出相對應的member資料
+Acompany.getByName = function(acomName, cb) {
   db.select()
     .from('acompany')
     .where({
       firmName : acomName
     })
     .map(function(row) {
-      //將select出來的資料轉換成Member物件
-      return new acom(row);
+      return new Acompany(row);
     })
     .then(function(acomList) {
       if(acomList.length) {
         cb(null, acomList[0]);
       } else {
-        //這邊要產生一個NotFound err給前端，因為error很常用到，我們會獨立出去一個檔案
         cb(new GeneralErrors.NotFound());
       }
     })
     .catch(function(err) {
       cb(err);
-    })
-}
-
-
-acom.getbyname = function(acomName, cb) {
-  db.select()
-    .from('acompany')
-    .where({
-      firmName : acomName
-    })
-    .map(function(row) {
-      return new acom(row);
-    })
-    .then(function(acomList) {
-      if(acomList.length) {
-        cb(null, acomList[0]);
-      } else {
-        cb(new GeneralErrors.NotFound());
-      }
-    })
-}
-
-acom.prototype.save = function (cb) {
-  //save的概念是當物件不存在時新增，存在時對DB做更新
-  if (this.firmName) {
-    //已存在
-    db("acompany")
-      .where({
-        firmName : this.firmName
-      })
-      .update({
-        componentID: this.componentID,
-        componentName : this.componentName,
-        componentQuantity : this.componentQuantity,
-        pumpCategory : this.pumpCategory,
-        address : this.address,
-        phoneNo : this.phoneNo,
-        personIC : this.personIC,
-        lendOT : this.lendOT,
-        returnT : this.returnT
-
-      })
-      .then(function() {
-        cb(null, this);
-      }.bind(this))
-      .catch(function(err) {
-        console.log("aCompany UPDATED", err);
-        cb(new GeneralErrors.Database());
-      });
-  } else {
-    //不存在
-    db("acompany")
-      .insert({
-        componentID: this.componentID,
-        componentName : this.componentName,
-        componentQuantity : this.componentQuantity,
-        pumpCategory : this.pumpCategory,
-        address : this.address,
-        phoneNo : this.phoneNo,
-        personIC : this.personIC,
-        lendOT : this.lendOT,
-        returnT : this.returnT
-      })
-      .then(function(result) {
-        var insertedName = result[0];
-        this.firmName = insertedName;
-        cb(null, this);
-      }.bind(this))
-      .catch(function(err) {
-        console.log("aCompany INSERT", err);
-        cb(new GeneralErrors.Database());
-      });
-  }
+    });
 };
+
+Acompany.prototype.insert = function(cb) {
+    console.log("insert");
+    db('acompany')
+        .insert({
+          firmName: this.firmName,
+          componentID: this.componentID,
+          componentName: this.componentName,
+          componentQuantity: this.componentQuantity,
+          address: this.address,
+          phoneNo: this.phoneNo,
+          personIC: this.personIC,
+          lendOT: this.lendOT,
+          returnT: this.returnT
+        })
+        .then(function(result) {
+          console.log("aaaa");
+            var insertedName = result[0];
+            this.firmName = insertedName;
+            cb(null, this);
+        }.bind(this))
+        .catch(function(err) {
+            console.log("ACOMPANY INSERT", err);
+            cb(new GeneralErrors.Database());
+        });
+
+};
+
+Acompany.prototype.update = function(cb) {
+    console.log("update");
+    db('acompany')
+        .update({
+          componentID: options.componentID,
+          componentName: options.componentName,
+          componentQuantity: options.componentQuantity,
+          address: options.address,
+          phoneNo: options.phoneNo,
+          personIC: options.personIC,
+          lendOT: options.lendOT,
+          returnT: options.returnT
+        })
+        .where({
+            firmName: options.firmName,
+        })
+        .then(function() {
+            cb(null);
+        })
+        .catch(function(err) {
+            console.log(err);
+            cb(null, new GeneralErrors.Database());
+        });
+};
+
+Acompany.prototype.delete = function(cb) {
+    console.log("delete");
+    db('acompany')
+        .where({
+            firmName: options.firmName
+        })
+        .del()
+        .then(function() {
+            cb(null);
+        })
+        .catch(function(err) {
+            console.log(err);
+            cb(null, new GeneralErrors.Database());
+        });
+};
+module.exports = Acompany;
--- routes/add.js
+++ routes/add.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 var Member = require('../models/Member');
 var Module = require('../models/Module');
+var Acompany = require('../models/Acompany');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if (req.session.member) {
         res.render('add', {
             member: req.session.member,
-            statusM: null
+            statusM: null,
+            statusA: null
         });
     } else {
         res.redirect('/');
@@ -37,24 +39,84 @@ router.post('/module', function(req, res, next) {
                         } else {
                             res.render('add', {
                                 member: req.session.member,
-                                statusM: '資料已新增!'
+                                statusM: '資料已新增!',
+                                statusA: null
                             });
                         }
                     });
                 } else {
+                    console.log(err.name);
                     next();
                 }
             } else {
-              console.log("error");
                 res.render('add', {
                     member: req.session.member,
-                    statusM: '您輸入的資料有誤!'
+                    statusM: '您輸入的資料有誤!',
+                    statusA: null
                 });
             }
-       });
+        });
     } else {
         res.redirect('/');
     }
 });
 
+router.post('/acom', function(req, res, next) {
+    if (req.session.member) {
+        var firmName = req.body.firmName;
+        var componentID = req.body.componentID;
+        var componentName = req.body.componentName;
+        var componentQuantity = req.body.componentQuantity;
+        var address = req.body.address;
+        var phoneNo = req.body.phoneNo;
+        var personIC = req.body.personIC;
+        var lendOT = req.body.lendOT;
+        var returnT = req.body.returnT;
+        console.log(firmName);
+
+        Acompany.getByName(firmName, function(err, acom) {
+            console.log("acom");
+            if (err) {
+                if (err.name == "NotFoundError") {
+                    var newAcompany = new Acompany({
+                        firmName: firmName,
+                        componentID: componentID,
+                        componentName: componentName,
+                        componentQuantity: componentQuantity,
+                        address: address,
+                        phoneNo: phoneNo,
+                        personIC: personIC,
+                        lendOT: lendOT,
+                        returnT: returnT
+                    });
+                    newAcompany.insert(function(err) {
+                        if (err) {
+                            console.log("ttt");
+                            next(err);
+                        } else {
+                            console.log("vvv");
+                            res.render('add', {
+                                member: req.session.member,
+                                statusM: null,
+                                statusA: '資料已新增!'
+                            });
+                        }
+                    });
+                } else {
+                    console.log(err.name + " d");
+                    next();
+                }
+            } else {
+                console.log("no err");
+                res.render('add', {
+                    member: req.session.member,
+                    statusM: null,
+                    statusA: '您輸入的資料有誤!'
+                });
+            }
+        });
+    } else {
+        res.redirect('/');
+    }
+});
 module.exports = router;
