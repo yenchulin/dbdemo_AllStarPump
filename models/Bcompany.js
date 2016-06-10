35ed009c50d864f57e11e1c00344a8a2109dbf6c
yenchulin <103306003@nccu.edu.tw>
2016-06-10 16:21:25 +0800

    add Bcom ejs

---
+++ models/Bcompany.js
@@ -0,0 +1,107 @@
+var db = require('../libs/db'); //引入我們的sql builder
+var GeneralErrors = require('../errors/GeneralErrors');
+
+
+var bcom = function(options) {
+    this.firmName = options.firmName;
+    this.pumpID = options.pumpID;
+    this.pumpUsage = options.pumpUsage;
+    this.pumpQuantity = options.pumpQuantity;
+    this.pumpCategory = options.pumpCategory;
+    this.pumpType = options.pumpType;
+    this.pumpFlow = options.pumpFlow;
+    this.personIC = options.personIC;
+    this.address = options.address;
+    this.phoneNo = options.phoneNo;
+    this.personIC = options.personIC;
+};
+
+//Class Function
+Bcompany.getByName = function(bcomName, cb) {
+    db.select()
+        .from('bcompany')
+        .where({
+            firmName: bcomName
+        })
+        .map(function(row) {
+            return new Bcompany(row);
+        })
+        .then(function(bcomList) {
+            if (bcomList.length) {
+                cb(null, bcomList[0]);
+            } else {
+                cb(new GeneralErrors.NotFound());
+            }
+        })
+        .catch(function(err) {
+            cb(err);
+        });
+};
+
+Bcompany.prototype.update = function(cb) {
+    console.log("update");
+    db("bcompany")
+        .where({
+            firmName: this.firmName
+        })
+        .update({
+            pumpID: this.pumpID,
+            pumpUsage: this.pumpUsage,
+            pumpQuantity: this.pumpQuantity,
+            pumpCategory: this.pumpCategory,
+            pumpType: this.pumpType,
+            pumpFlow: this.pumpFlow,
+            address: this.address,
+            phoneNo: this.phoneNo,
+            personIC: this.personIC
+        })
+        .then(function() {
+            cb(null, this);
+        }.bind(this))
+        .catch(function(err) {
+            console.log("BCOMPANY UPDATED", err);
+            cb(new GeneralErrors.Database());
+        });
+};
+
+Bcompany.prototype.insert = function(cb) {
+    console.log("insert");
+    db("bcompany")
+        .insert({
+            pumpID: this.pumpID,
+            pumpUsage: this.pumpUsage,
+            pumpQuantity: this.pumpQuantity,
+            pumpCategory: this.pumpCategory,
+            pumpType: this.pumpType,
+            pumpFlow: this.pumpFlow,
+            address: this.address,
+            phoneNo: this.phoneNo,
+            personIC: this.personIC
+        })
+        .then(function(result) {
+            var insertedName = result[0];
+            this.firmName = insertedName;
+            cb(null, this);
+        }.bind(this))
+        .catch(function(err) {
+            console.log("BCOMPANY INSERT", err);
+            cb(new GeneralErrors.Database());
+        });
+};
+
+Bcompany.prototype.delete = function(cb) {
+    console.log("delete");
+    db('bcompany')
+        .where({
+            firmName: this.firmName
+        })
+        .del()
+        .then(function() {
+            cb(null);
+        })
+        .catch(function(err) {
+            console.log(err);
+            cb(null, new GeneralErrors.Database());
+        });
+};
+module.exports = Bcompany;
