78ab4e7803910be745a3eaf65632f73ce6d95115
黃秋瑾 <huangchioujiin@huangqiujinde-MacBook-Pro.local>
2016-06-13 06:12:01 +0800

    qqqq

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
+ $('#moduleQuantity').click(function(){
+     $(this).hide();
+     $('#hiddenForm1').show();
+ });
+ $('#cancel1').click(function(){
+     $('#hiddenForm1').hide();
+     $('#moduleQuantity').show();
+ });
+ });
+ $(document).ready(function() {
 
+ $('#moduleUsage').click(function(){
+     $(this).hide();
+     $('#hiddenForm').show();
+ });
+ $('#cancel').click(function(){
+     $('#hiddenForm').hide();
+     $('#moduleUsage').show();
+ });
+});
+ $(document).ready(function() {
+
+ $('#acompanyName').click(function(){
+     $(this).hide();
+     $('#hiddenForm2').show();
+ });
+ $('#cancel2').click(function(){
+     $('#hiddenForm2').hide();
+     $('#acompanyName').show();
+ });
+});
+ $(document).ready(function() {
+
+ $('#quantityOwned').click(function(){
+     $(this).hide();
+     $('#hiddenForm3').show();
+ });
+ $('#cancel3').click(function(){
+     $('#hiddenForm3').hide();
+     $('#quantityOwned').show();
+ });
 });
--- routes/moduleUpdate.js
+++ routes/moduleUpdate.js
@@ -2,10 +2,35 @@ var express = require('express');
 var router = express.Router();
 var Module = require('../models/Module');
 
-router.post('/',function(res,req,next){
-  
-  res.render('moduleResult',{
-    member: req.session.member
-  });
+router.post('/', function(req, res, next) {
+    var moduleId = req.body.moduleId;
+    var moduleUsage = req.body.moduleUsage;
+    var moduleQuantity = req.body.moduleQuantity;
+    var acompanyName = req.body.acompanyName;
+    var quantityOwned = req.body.quantityOwned;
+    console.log(req.body);
+
+    Module.getById(moduleId, function(err, module) {
+        if (err) {
+            console.log(err.name);
+            next(err);
+        } else {
+            var updtModule = new Module({
+                moduleId: moduleId,
+                moduleUsage: moduleUsage,
+                moduleQuantity: moduleQuantity,
+                acompanyName: acompanyName,
+                quantityOwned: quantityOwned
+            });
+            console.log(updtModule);
+            updtModule.update(function(err) {
+                if (err) {
+                    next(err);
+                } else {
+                    res.redirect('/query');
+                }
+            });
+        }
+    });
 });
 module.exports = router;
